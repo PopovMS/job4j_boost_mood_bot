9cee14f2bdb76cb439bf612fdd8ec7ac57f3bee0
Misha <m.popov83@gmail.com>
2025-02-03 09:12:47 +0500

    Жизненный цикл Spring Bean [#505292]

--- src/main/java/ru/job4j/services/BotCommandHandler.java
+++ src/main/java/ru/job4j/services/BotCommandHandler.java
@@ -1,11 +1,23 @@
 package ru.job4j.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
 import ru.job4j.content.Content;
 
 @Service
 public class BotCommandHandler {
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through @PostConstruct init.");
+    }
+
     void receive(Content content) {
         System.out.println(content);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed via @PreDestroy.");
     }
 }
