a4b627c3cf8424f0821f1026556f41417f4b277d
Misha <m.popov83@gmail.com>
2025-02-04 10:30:49 +0500

    Spring Data JPA [#505298]

--- src/main/java/ru/job4j/store/UserRepository.java
+++ src/main/java/ru/job4j/store/UserRepository.java
@@ -7,8 +7,4 @@ import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findAll();
-
-    User findByClientId(Long clientId);
-
-    public Object save(User user);
 }
