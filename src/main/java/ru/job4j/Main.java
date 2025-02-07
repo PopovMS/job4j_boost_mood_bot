bfeb60b006a5c32da7aa3636cbf7ab0700a46c8b
Misha <m.popov83@gmail.com>
2025-02-07 18:04:37 +0500

    @Bean | Инициализация базы данных [#505304]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -9,7 +9,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.job4j.model.*;
 import ru.job4j.services.TgRemoteService;
+import ru.job4j.store.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 @EnableScheduling
 @SpringBootApplication
@@ -19,16 +27,47 @@ public class Main {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    CommandLineRunner loadDatabase(MoodRepository moodRepository,
+                                   MoodContentRepository moodContentRepository,
+                                   AwardRepository awardRepository) {
         return args -> {
-            var bot = ctx.getBean(TgRemoteService.class);
-            var botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            try {
-                botsApi.registerBot(bot);
-                System.out.println("Бот успешно зарегистрирован");
-            } catch (TelegramApiException e) {
+            var moods = moodRepository.findAll();
+            if (!moods.isEmpty()) {
+                return;
+            }
+            var data = new ArrayList<MoodContent>();
+            try (
+                    BufferedReader read = new BufferedReader(new FileReader("./MoodContent.txt"))) {
+                read.lines()
+                        .map(s -> s.split(":"))
+                        .filter(array -> array.length > 1)
+                        .forEach(vol -> {
+                            data.add(new MoodContent(new Mood(vol[0], true), vol[1]));
+                                }
+                        );
+            } catch (
+                    IOException e) {
                 e.printStackTrace();
             }
+            moodRepository.saveAll(data.stream().map(MoodContent::getMood).toList());
+            moodContentRepository.saveAll(data);
+
+            // Инициализация наград
+            var awards = new ArrayList<Award>();
+            try (
+                    BufferedReader read = new BufferedReader(new FileReader("./Award.txt"))) {
+                read.lines()
+                        .map(s -> s.split(":"))
+                        .filter(array -> array.length > 1)
+                        .forEach(vol -> {
+                                    awards.add(new Award(vol[0], vol[1], Integer.parseInt(vol[2])));
+                                }
+                        );
+            } catch (
+                    IOException e) {
+                e.printStackTrace();
+            }
+            awardRepository.saveAll(awards);
         };
     }
 }
