ddbe59647aec08f1769c21ae8a294ca0bf5268d7
Misha <m.popov83@gmail.com>
2025-02-12 09:33:01 +0500

    @Service и @Repository | BotCommandHandler - обработка комманд [#505306]

--- src/main/java/ru/job4j/services/MoodService.java
+++ src/main/java/ru/job4j/services/MoodService.java
@@ -3,10 +3,13 @@ package ru.job4j.services;
 
 import org.springframework.stereotype.Service;
 import ru.job4j.content.*;
+import ru.job4j.model.Achievement;
+import ru.job4j.model.Mood;
 import ru.job4j.model.MoodLog;
 import ru.job4j.model.User;
 import ru.job4j.store.*;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -33,34 +36,73 @@ public class MoodService {
     }
 
     public Content chooseMood(User user, Long moodId) {
+        Mood mood = new Mood();
+        mood.setId(moodId);
+        MoodLog moodLog = new MoodLog();
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        long currentDateTimeFormat = Long.parseLong(currentDateTime.format(formatter));
+        moodLog.setMood(mood);
+        moodLog.setUser(user);
+        moodLog.setCreatedAt(currentDateTimeFormat);
+        moodLogRepository.save(moodLog);
         return recommendationEngine.recommendFor(user.getChatId(), moodId);
     }
 
     public Optional<Content> weekMoodLogCommand(long chatId, Long clientId) {
+        LocalDateTime weeklyLog = LocalDateTime.now().minusDays(7);
+        long weeklyLogDate = Long.parseLong(weeklyLog.format(formatter));
+        List<MoodLog> filteredLog = moodLogRepository.findAll().stream()
+                .filter(vol -> vol.getUser().getClientId() == clientId)
+                .filter(vol -> vol.getCreatedAt() >= weeklyLogDate)
+                .toList();
         var content = new Content(chatId);
+        content.setText(formatMoodLogs(filteredLog, "Список настроений за 7 дней"));
         return Optional.of(content);
     }
 
     public Optional<Content> monthMoodLogCommand(long chatId, Long clientId) {
+        LocalDateTime monthlyLog = LocalDateTime.now().minusDays(30);
+        long monthlyLogDate = Long.parseLong(monthlyLog.format(formatter));
+        List<MoodLog> filteredLog = moodLogRepository.findAll().stream()
+                .filter(vol -> vol.getUser().getClientId() == clientId)
+                .filter(vol -> vol.getCreatedAt() >= monthlyLogDate)
+                .toList();
         var content = new Content(chatId);
+        content.setText(formatMoodLogs(filteredLog, "Список настроений за месяц"));
         return Optional.of(content);
     }
 
     private String formatMoodLogs(List<MoodLog> logs, String title) {
         if (logs.isEmpty()) {
-            return title + ":\nNo mood logs found.";
+            return String.format("%s:%n%s", title, "Список настроений не найден");
         }
-        var sb = new StringBuilder(title + ":\n");
+        var result = new StringBuilder(String.format("%s%n", title));
         logs.forEach(log -> {
             String formattedDate = formatter.format(Instant.ofEpochSecond(log.getCreatedAt()));
-            sb.append(formattedDate).append(": ").append(log.getMood().getText()).append("\n");
+            result.append(formattedDate).append(": ").append(log.getMood().getText()).append(System.lineSeparator());
         });
-        return sb.toString();
+        return result.toString();
     }
 
     public Optional<Content> awards(long chatId, Long clientId) {
+        List<Achievement> achievements = achievementRepository.findAll().stream()
+                .filter(vol -> vol.getUser().getClientId() == clientId)
+                .toList();
         var content = new Content(chatId);
+        content.setText(formatAwardLogs(achievements, "Награды"));
         return Optional.of(content);
+    }
+
+    private String formatAwardLogs(List<Achievement> logs, String title) {
+        if (logs.isEmpty()) {
+            return String.format("%s:%n%s", title, "Награды не найдены");
+        }
+        var result = new StringBuilder(String.format("%s%n", title));
+        logs.forEach(log -> {
+            String formattedDate = formatter.format(Instant.ofEpochSecond(log.getCreateAt()));
+            result.append(formattedDate).append(": ").append(log.getAward().getTitle()).append(System.lineSeparator());
+        });
+        return result.toString();
     }
 }
 
--- src/main/java/ru/job4j/services/TgRemoteService.java
+++ src/main/java/ru/job4j/services/TgRemoteService.java
@@ -5,15 +5,11 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.job4j.model.User;
 import ru.job4j.store.MoodRepository;
 import ru.job4j.store.UserRepository;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Service
