a8c45c9c9e274324f33096ae10414e042fe9bd82
Misha <m.popov83@gmail.com>
2025-02-12 10:50:20 +0500

    @Service и @Repository | BotCommandHandler - обработка комманд [#505306]

--- src/main/java/ru/job4j/store/MoodRepository.java
+++ src/main/java/ru/job4j/store/MoodRepository.java
@@ -3,14 +3,10 @@ package ru.job4j.store;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.model.Mood;
-import ru.job4j.model.User;
-
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface MoodRepository extends CrudRepository<Mood, Long> {
     List<Mood> findAll();
-
-    Optional<Mood> findByMoodId(Long id);
 }
