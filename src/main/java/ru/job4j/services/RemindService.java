dafdb655310b1ee648854314c33421d352698163
Misha <m.popov83@gmail.com>
2025-02-15 11:06:33 +0500

    SentContent - интеграция Content с Telegram API [#505307]

--- src/main/java/ru/job4j/services/RemindService.java
+++ src/main/java/ru/job4j/services/RemindService.java
@@ -4,15 +4,16 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import ru.job4j.content.Content;
+import ru.job4j.sending.SentContent;
 import ru.job4j.store.UserRepository;
 
 @Service
 public class RemindService {
-    private final TelegramBotService telegramBotService;
+    private final SentContent sentContent;
     private final UserRepository userRepository;
 
-    public RemindService(TelegramBotService telegramBotService, UserRepository userRepository) {
-        this.telegramBotService = telegramBotService;
+    public RemindService(SentContent sentContent, UserRepository userRepository) {
+        this.sentContent = sentContent;
         this.userRepository = userRepository;
     }
 
@@ -21,7 +22,7 @@ public class RemindService {
         for (var user : userRepository.findAll()) {
             var content = new Content(user.getChatId());
             content.setText("Ping");
-            telegramBotService.sent(content);
+            sentContent.sent(content);
         }
     }
 }
