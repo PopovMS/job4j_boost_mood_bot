679c185be605076d98d125a39e050aa40857229d
Misha <m.popov83@gmail.com>
2025-02-15 18:23:48 +0500

    RemindService | Тестирование [#505308]

--- src/main/java/ru/job4j/store/MoodLogRepository.java
+++ src/main/java/ru/job4j/store/MoodLogRepository.java
@@ -7,7 +7,9 @@ import ru.job4j.model.User;
 import java.util.List;
 
 @Repository
+
 public interface MoodLogRepository extends CrudRepository<MoodLog, Long> {
     List<MoodLog> findAll();
-    List<User> findUsersWhoDidNotVoteToday(long startOfDay, long endOfDay);
+
+    List<User> findUsersWhoDidNotVoteToday (long startOfDay, long endOfDay);
 }
