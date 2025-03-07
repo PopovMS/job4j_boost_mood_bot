af6100cbfcfd6edac26b6e09a503c178520c8f34
Misha <m.popov83@gmail.com>
2025-03-07 10:31:52 +0500

    @Conditional [#505316 #534175]

--- src/main/java/ru/job4j/services/TelegramBotFakeService.java
+++ src/main/java/ru/job4j/services/TelegramBotFakeService.java
@@ -3,20 +3,20 @@ package ru.job4j.services;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.meta.generics.BotOptions;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import ru.job4j.config.OnFakeCondition;
 import ru.job4j.content.Content;
 import ru.job4j.sending.SentContent;
 
 @Service("TelegramBot")
 @Conditional(OnFakeCondition.class)
-public class TelegramBotFakeService extends TelegramLongPollingBot implements SentContent {
+public class TelegramBotFakeService implements SentContent, LongPollingBot {
     private final String botName;
 
-    public TelegramBotFakeService(@Value("${telegram.bot.name}") String botName,
-                                    @Value("${telegram.bot.token}") String botToken) {
-        super(botToken);
+    public TelegramBotFakeService(@Value("${telegram.bot.name}") String botName) {
         this.botName = botName;
     }
 
@@ -29,7 +29,22 @@ public class TelegramBotFakeService extends TelegramLongPollingBot implements Se
     }
 
     @Override
+    public BotOptions getOptions() {
+        return null;
+    }
+
+    @Override
+    public void clearWebhook() throws TelegramApiRequestException {
+
+    }
+
+    @Override
     public String getBotUsername() {
         return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
     }
 }
